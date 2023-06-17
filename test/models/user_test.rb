9ffe0e440a74ea09fa40d7b858dc42918db78ce7
mykoladev0220 <mykolademchukdev@gmail.com>
2023-06-17 23:21:29 +0300

    Update model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,6 +62,9 @@ class UserTest < ActiveSupport::TestCase
     cart.cart_items.first.update(quantity: 1)
     assert_equal 1, cart.cart_items.first.quantity
     assert_equal 2, iphone.reload.quantity
+
+    cart.cart_items.first.update(quantity: 2)
+    assert_equal 2, cart.cart_items.first.quantity
   end
 
   test 'user can not update the quantity of an item in the cart if the quantity is greater than the stock' do
@@ -83,7 +86,7 @@ class UserTest < ActiveSupport::TestCase
     assert_equal 3, cart.cart_items.first.quantity
     assert_equal 0, iphone.reload.quantity
 
-    cart.cart_items.first.destroy
+    cart.cart_items.first.remove_from_cart
 
     assert_equal 0, cart.cart_items.count
     assert_equal 3, iphone.reload.quantity
@@ -96,5 +99,26 @@ class UserTest < ActiveSupport::TestCase
     assert_equal 3, iphone.reload.quantity
 
     assert_equal 'Quantity must be greater than or equal to 1', create.errors.full_messages.first
+  end
+
+  test 'user can get the total price of the cart' do
+    CartItem.create(cart:, item: iphone, quantity: 3)
+    CartItem.create(cart:, item: ruby_book, quantity: 2)
+
+    assert_equal 400, cart.total
+  end
+
+  test 'user can checkout and the cart is emptied' do
+    CartItem.create(cart:, item: iphone, quantity: 3)
+    CartItem.create(cart:, item: ruby_book, quantity: 3)
+
+    assert_equal 450, cart.total
+
+    user.cart.checkout
+    assert_equal 0, cart.cart_items.count
+    assert_equal 0, cart.total
+
+    assert_equal 0, iphone.reload.quantity
+    assert_equal 0, ruby_book.reload.quantity
   end
 end
