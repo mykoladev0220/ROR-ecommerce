4ae29e9d669c9c00118481b36f6fcde46fc3eb55
mykoladev0220 <mykolademchukdev@gmail.com>
2023-06-19 21:49:03 +0300

    Write system test for UX

---
+++ test/system/user_shopping_cart_experience_test.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class UserShoppingCartExperienceTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  attr_accessor :user
+
+  setup do
+    @user = users(:one)
+    sign_in user
+
+    visit root_url
+  end
+
+  test 'should sign up' do
+    click_on 'Log out'
+
+    click_on 'Sign up'
+
+    fill_in 'user_email', with: 'minitest@test.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+
+    within('#new_user') do
+      click_on 'Sign up'
+    end
+
+    assert_text 'Welcome! You have signed up successfully.'
+  end
+
+  test 'should add item to cart' do
+    # Add item to cart
+    click_on 'ruby book'
+    click_on 'Add item to cart'
+
+    assert_text 'Item added to cart.'
+    assert_text 'Cart | 1 items'
+  end
+
+  test 'should update cart item and remove cart item' do
+    click_on 'ruby book'
+    click_on 'Add item to cart'
+
+    assert_text 'Item added to cart.'
+    assert_text 'Cart | 1 items'
+
+    # Go to cart
+    click_on 'Cart | 1 items'
+
+    assert_text 'Cart total: $50'
+
+    # Update quantity
+    fill_in 'cart_item_quantity', with: 3
+    click_on 'Update quantity'
+
+    assert_text 'Cart item updated.'
+    assert_text 'Cart total: $150'
+
+    # Remove item from cart
+    click_on 'Remove from cart'
+
+    assert_text 'Cart item deleted.'
+    assert_text 'Cart | 0 items'
+    assert_text 'Cart total: $0'
+  end
+
+  test 'should not checkout an empty cart' do
+    # Try visiting checkout page with empty cart
+    visit checkout_cart_url
+
+    assert_text 'You cannot checkout an empty cart.'
+  end
+
+  test 'should checkout if cart is not empty' do
+    # Add item to cart
+    click_on 'ruby book'
+    click_on 'Add item to cart'
+
+    assert_text 'Item added to cart.'
+    assert_text 'Cart | 1 items'
+
+    # Go to cart
+    click_on 'Cart | 1 items'
+
+    assert_text 'Cart total: $50'
+
+    # Checkout
+    click_on 'Go to checkout'
+
+    fill_in 'checkout_cart_number', with: '4242424242424242'
+    fill_in 'checkout_cart_expiration_month', with: '12'
+    fill_in 'checkout_cart_expiration_year', with: '2025'
+    fill_in 'checkout_cart_cvv', with: '123'
+    fill_in 'checkout_shipping_address', with: '123 Main St.'
+    click_on 'Submit Payment'
+
+    assert_text 'Checkout successful.'
+    assert_text 'Cart | 0 items'
+    assert_text 'Bought Items'
+    assert_text 'Total Spending: $50'
+  end
+end
