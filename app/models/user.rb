9766250235139875aee4fcfb75085111a7b9691b
mykoladev0220 <mykolademchukdev@gmail.com>
2023-06-21 13:29:57 +0300

    Move callback to down

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create :create_cart
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
@@ -31,6 +29,8 @@ class User < ApplicationRecord
   has_one :cart, dependent: :destroy
   has_many :cart_items, through: :cart
   has_many :bought_items, dependent: :destroy
+
+  after_create :create_cart
 
   private
 
